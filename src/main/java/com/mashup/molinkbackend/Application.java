42fb4ade1f13e8dbe2b325e950f47531a23b551d
leemirim <leemirim9654@gmail.com>
2019-06-30 01:43:28 +0900

    [MODIFY] Apllication 파일명 수정

--- src/main/java/com/mashup/molinkbackend/Application.java
+++ src/main/java/com/mashup/molinkbackend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MolinkBackendApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(MolinkBackendApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
