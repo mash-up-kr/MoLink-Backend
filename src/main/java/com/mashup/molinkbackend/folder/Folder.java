10883819f88ec6e708c8f9c560eea381a8b6fb6b
leemirim <leemirim9654@gmail.com>
2019-06-30 03:36:58 +0900

    [MODIFY] Folder 엔티티 수정

--- src/main/java/com/mashup/molinkbackend/folder/Folder.java
+++ src/main/java/com/mashup/molinkbackend/folder/Folder.java
@@ -1,4 +1,34 @@
 package com.mashup.molinkbackend.folder;
 
-public class Folder {
+import com.mashup.molinkbackend.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Folder extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 40)
+    private String name;
+
+    @Column(length = 20)
+    private String color;
+
+    private Integer parentId;
+
+    @Builder
+    public Folder(String name, String color, Integer parentId) {
+        this.name = name;
+        this.color = color;
+        this.parentId = parentId;
+    }
 }
