262ee6d3ce528fc01ab30efe6c0d2e6eb5c7da56
MisunJoo <0987someday@naver.com>
2019-06-29 17:47:20 +0900

    [ADD] First Commit

---
+++ src/main/java/com/mashup/molinkbackend/MolinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.molinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolinkBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MolinkBackendApplication.class, args);
+  }
+
+}
