6f8706285325177aeee052d93a8c16fc98f639a9
leemirim <leemirim9654@gmail.com>
2019-07-11 16:19:12 +0900

    FolderTest 생성 및 FolderService 인터페이스 삭제와 FolderService 클래스 생성

--- src/main/java/com/mashup/molinkbackend/folder/FolderService.java
+++ src/main/java/com/mashup/molinkbackend/folder/FolderService.java
@@ -1,5 +1,21 @@
 package com.mashup.molinkbackend.folder;
 
-public interface FolderService {
-    void save(ReqSaveFolderDto folderSaveRequestDto);
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FolderServiceImpl implements FolderService {
+
+    private final FolderRepository folderRepository;
+
+    @Override
+    public void save(ReqSaveFolderDto folderSaveRequestDto) {
+        Folder folder = Folder.builder()
+                .name(folderSaveRequestDto.getName())
+                .color(folderSaveRequestDto.getColor())
+                .parentId(folderSaveRequestDto.getParentId())
+                .build();
+        folderRepository.save(folder);
+    }
 }
